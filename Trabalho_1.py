c400c0dbc7ddc52d2324de6f9db2c8247af83198
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 18:29:45 -0300

    Request of the valor_base
    Request for plan base value

--- Trabalho_1.py
+++ Trabalho_1.py
@@ -1,2 +1,7 @@
 # CODE REQUIREMENT 1 of 6
 print("Bem-vindo ao sistema da Sammy Health! Desenvolvido por Samuel Alves Miranda.")
+
+# CODE REQUIREMENT 1 of 2 - price based on age
+
+# request the base value of the heath plan
+valor_base = float(input("Digite o valor base do plano: R$ "))
