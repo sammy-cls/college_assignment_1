650459fae2b01e499d13e274b85a964697adfab8
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 18:33:25 -0300

    Request the client age
    Idade = Age
    Brazilian Portuguese comments update

--- Trabalho_1.py
+++ Trabalho_1.py
@@ -1,7 +1,10 @@
-# CODE REQUIREMENT 1 of 6
+# CODE REQUIREMENT 1 of 6 (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 6)
 print("Bem-vindo ao sistema da Sammy Health! Desenvolvido por Samuel Alves Miranda.")
 
-# CODE REQUIREMENT 1 of 2 - price based on age
+# CODE REQUIREMENT 1 of 2 - price based on age (pt-br: EXIGÊNCIA DO CÓDIGO 2 de 6 - preço baseado na idade)
 
-# request the base value of the heath plan
+# request the base value of the heath plan (pt-br: solicitar o valor base do plano de saúde)
 valor_base = float(input("Digite o valor base do plano: R$ "))
+
+# request the client age (pt-br: solicitar a idade do cliente)
+idade = int(input("Digite a idade do cliente: "))
