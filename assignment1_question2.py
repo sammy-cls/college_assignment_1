15b7dda2b620561754ab3bcc47ddb57a974c3e93
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 19:42:41 -0300

    Update assignment1_question2.py

--- assignment1_question2.py
+++ assignment1_question2.py
@@ -1,7 +1,7 @@
 # CODE REQUIREMENT 1 of 8 - welcome message with name, surname, and menu (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 8 - mensagem de boas-vindas com nome, sobrenome e menu)
 
-print("=" * 50)
-print("Bem-vindo à Pizzaria do Samuel Alves Miranda!")
-print("=" * 50)
-print('-' * 18 + "Cardápio" + '-' * 18)
-print("-" * 44)
+print("=" * 64)
+print('=' * 9 + "Bem-vindo à Pizzaria do Samuel Alves Miranda!" + '=' * 10 )
+print("=" * 64)
+print('-' * 28 + "Cardápio" + '-' * 28)
+print("-" * 64)
