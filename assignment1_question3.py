265b49d385a98f82a95639c2b12cec87b6cceec2
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 21:29:47 -0300

    Create assignment1_question3.py

---
+++ assignment1_question3.py
@@ -0,0 +1,3 @@
+# CODE REQUIREMENT 1 of 7 - welcome message with name and surname (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 7 - mensagem de boas-vindas com nome e sobrenome)
+
+print("Bem-vindo à Madeirereira do lenhador Samuel Alves Miranda!")
