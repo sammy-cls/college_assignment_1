a98aa8db5b8b942ce0b800ca8898af7aa34a7436
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 21:34:47 -0300

    Update assignment1_question3.py

--- assignment1_question3.py
+++ assignment1_question3.py
@@ -1,3 +1,16 @@
 # CODE REQUIREMENT 1 of 7 - welcome message with name and surname (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 7 - mensagem de boas-vindas com nome e sobrenome)
 
 print("Bem-vindo à Madeirereira do lenhador Samuel Alves Miranda!")
+
+# CODE REQUIREMENT 2 of 7 - def of wood type (pt-br: def de tipo de madeira)
+
+def escolha_tipo():
+    valoresMadeira = {
+        "PIN": 150.40, # pinho
+        "PER": 170.20, # peroba
+        "MOG": 190.90, # mogno
+        "IPE": 210.10, # ipê
+        "IMB": 220.70 # imbuia
+    }
+
+    while True:
