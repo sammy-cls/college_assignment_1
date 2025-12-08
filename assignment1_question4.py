dadc7cf72da44563229871fa681dc9569963df68
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-08 19:35:54 -0300

    Update assignment1_question4.py

--- assignment1_question4.py
+++ assignment1_question4.py
@@ -1,5 +1,11 @@
 # Business Contact Management Software
 
+# CODE REQUIREMENT 1 of 8 welcome message with name and surname
+# (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 8 - mensagem de boas-vindas com nome e sobrenome)
+
 print('=' * 4 + " Bem-vindo à lista de Contatos do Samuel Alves Miranda! " + '=' * 4)
 print("=" * 64)
 print('-' * 25 + "Menu Principal" + '-' * 25)
+
+# CODE REQUIREMENT 2 of 8 - list of contacts and variable id_global with RU value
+# (pt-br: EXIGÊNCIA DO CÓDIGO 2 de 8 - lista_contatos e a variável id_global com valor igual ao número de meu RU)
