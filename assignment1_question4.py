672fb910cc6824d41d40f5ae2d4fda69c5bc7503
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-08 19:32:56 -0300

    Create assignment1_question4.py

---
+++ assignment1_question4.py
@@ -0,0 +1,5 @@
+# Business Contact Management Software
+
+print('=' * 4 + " Bem-vindo à lista de Contatos do Samuel Alves Miranda! " + '=' * 4)
+print("=" * 64)
+print('-' * 25 + "Menu Principal" + '-' * 25)
