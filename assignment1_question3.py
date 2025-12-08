c36535bf01470a28177ff8f9e05c71e4b271a84f
sammy <samuelalvesmirandacomercial@gmail.com>
2025-12-07 22:39:59 -0300

    Update assignment1_question3.py

--- assignment1_question3.py
+++ assignment1_question3.py
@@ -1,6 +1,6 @@
 # CODE REQUIREMENT 1 of 7 - welcome message with name and surname (pt-br: EXIGÊNCIA DO CÓDIGO 1 de 7 - mensagem de boas-vindas com nome e sobrenome)
 
-print("Bem-vindo à Madeirereira do lenhador Samuel Alves Miranda!")
+print("Bem-vindo à Madeireira do lenhador Samuel Alves Miranda!")
 
 # CODE REQUIREMENT 2 of 7 - def of wood type (pt-br: def de tipo de madeira)
 
